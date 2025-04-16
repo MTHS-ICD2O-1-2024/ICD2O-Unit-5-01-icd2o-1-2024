7315185cf7369b0875d80ed670a5564df78837c5
icd2o-1-2024 <icd2o-1-2024@mths.ca>
2025-04-16 14:49:37 +0000

    linting fix

--- js/script.js
+++ js/script.js
@@ -4,26 +4,26 @@
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
 
-const randomNumber = Math.floor(Math.random() *6) + 1
+const randomNumber = Math.floor(Math.random() * 6) + 1
 
 /**
  * This function checks if the user has selected the random number from above
  */
-function checkNumber() {
+function checkNumber () {
   // input
-  const userNumber = parseInt(document.getElementById("user-number").value)
+  const userNumber = parseInt(document.getElementById('user-number').value)
 
-  //process
-  if (userNumber == randomNumber) {
+  // process
+  if (userNumber === randomNumber) {
     // output
-    document.getElementById("result").innerHTML =
-      "<p>You have guessed the correct number!</p>"
+    document.getElementById('result').innerHTML =
+      '<p>You have guessed the correct number!</p>'
   }
 
-  if (userNumber != randomNumber) {
+  if (userNumber !== randomNumber) {
     // output
-    document.getElementById("result").innerHTML =
-      "<p>You have guessed the wrong number!" +
-      "<br/>The correct number was: " + randomNumber + "</p>"
+    document.getElementById('result').innerHTML =
+      '<p>You have guessed the wrong number!' +
+      '<br/>The correct number was: ' + randomNumber + '</p>'
   }
 }
