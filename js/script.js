9820e29ea79486f6b4791741c758e99a78c15ac6
icd2o-1-2024 <icd2o-1-2024@mths.ca>
2025-04-16 14:44:03 +0000

    init

--- js/script.js
+++ js/script.js
@@ -3,3 +3,27 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+const randomNumber = Math.floor(Math.random() *6) + 1
+
+/**
+ * This function checks if the user has selected the random number from above
+ */
+function checkNumber() {
+  // input
+  const userNumber = parseInt(document.getElementById("user-number").value)
+
+  //process
+  if (userNumber == randomNumber) {
+    // output
+    document.getElementById("result").innerHTML =
+      "<p>You have guessed the correct number!</p>"
+  }
+
+  if (userNumber != randomNumber) {
+    // output
+    document.getElementById("result").innerHTML =
+      "<p>You have guessed the wrong number!" +
+      "<br/>The correct number was: " + randomNumber + "</p>"
+  }
+}
